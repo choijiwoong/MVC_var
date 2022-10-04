4199b3bfd82932c9b1448ec14bdd5e31eead126a
Choijiwoong <gogogi313@naver.com>
2022-10-04 21:03:34 +0900

    [DOCS] 초기 파일 업로드

---
+++ src/main/java/com/holly/molly/GodApplication.java
@@ -0,0 +1,13 @@
+package com.holly.molly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GodApplication.class, args);
+	}
+
+}
